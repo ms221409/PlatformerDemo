5c3cb695b25c993adac16062d6e6dd0d8d974e12
Michael Stephens <ms221409@scc-mmlab-ins.local>
2019-10-07 16:20:45 -0400

    Initial commit

---
+++ Platformer/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    //
+    public void LoadLevelFromUI (int levelIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ Platformer/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject menuObject;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            menuObject.SetActive(!menuObject.activeSelf);
+        }
+    }
+
+
+    public void LoadMainMenu ()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
